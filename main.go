7b41028ffd87584c6281175f113ed63128a91c09
Amirabbas Fazelinia <utilyre@gmail.com>
2024-08-17 23:45:17 +0330

    refactor: merge packages command and main

--- main.go
+++ main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
-	"log"
+	"context"
+	"errors"
+	"fmt"
 	"os"
 
-	"gihtub.com/utilyre/summer/command"
+	"gihtub.com/utilyre/summer/pkg/summer"
 )
 
 func main() {
-	log.SetFlags(0)
-	if err := command.Execute(os.Args[1:]); err != nil {
-		log.Fatalf("summer: %s", err)
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "summer: %v\n", err)
 	}
+}
+
+func run() error {
+	if len(os.Args) < 2 {
+		return errors.New("too few arguments")
+	}
+	if len(os.Args) > 2 {
+		return errors.New("too many arguments")
+	}
+
+	checksums, err := summer.SumTree(context.Background(), os.Args[1])
+	if err != nil {
+		return err
+	}
+
+	for _, cs := range checksums {
+		fmt.Printf("%x  %s\n", cs.Hash, cs.Name)
+	}
+
+	return nil
 }
