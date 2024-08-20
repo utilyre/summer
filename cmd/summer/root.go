39f992fb17424e6fd981611262697a995d1198e5
Amirabbas Fazelinia <utilyre@gmail.com>
2024-08-20 10:55:37 +0330

    fix(cmd): silence auto generated help on error

--- cmd/summer/root.go
+++ cmd/summer/root.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 	"github.com/utilyre/summer/pkg/summer"
 )
@@ -15,8 +12,10 @@ var (
 
 func newCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "summer",
-		Short: "High-performance utility for generating checksums in parallel",
+		Use:           "summer",
+		Short:         "High-performance utility for generating checksums in parallel",
+		SilenceErrors: true,
+		SilenceUsage:  true,
 	}
 	fset := cmd.PersistentFlags()
 	fset.Int64Var(&timeout, "timeout", 0, "cancel operation after given milliseconds")
@@ -25,7 +24,7 @@ func newCommand() *cobra.Command {
 	cmdGenerate := &cobra.Command{
 		Use:   "generate [files]",
 		Short: "Generate checksums for given files",
-		Run:   wrapRun(generate),
+		RunE:  generate,
 	}
 	fset = cmdGenerate.Flags()
 	fset.Int("read-jobs", 1, "run given number of read jobs in parallel")
@@ -33,13 +32,4 @@ func newCommand() *cobra.Command {
 
 	cmd.AddCommand(cmdGenerate)
 	return cmd
-}
-
-func wrapRun(run func(*cobra.Command, []string) error) func(*cobra.Command, []string) {
-	return func(cmd *cobra.Command, args []string) {
-		if err := run(cmd, args); err != nil {
-			fmt.Fprintf(os.Stdout, "%s: %v\n", cmd.Name(), err)
-			os.Exit(1)
-		}
-	}
 }
