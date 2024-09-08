de07ad380479b8113f247a28334855acb2589a69
Amirabbas Fazelinia <utilyre@gmail.com>
2024-09-08 21:54:07 +0330

    refactor(pipeline)!: rename interface Pipe to Stage

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -2,55 +2,55 @@ package pipeline
 
 import "context"
 
-// A Pipe is the smallest unit capable of processing data concurrently.
-type Pipe[T any] interface {
+// A Stage is the smallest unit capable of processing data concurrently.
+type Stage[T any] interface {
 	Pipe(ctx context.Context, in <-chan T) (out <-chan T)
 }
 
-// A PipeFunc is an adapter to allow the use of ordinary functions as Pipe.
-type PipeFunc[T any] func(ctx context.Context, in <-chan T) <-chan T
+// A StageFunc is an adapter to allow the use of ordinary functions as Stage.
+type StageFunc[T any] func(ctx context.Context, in <-chan T) <-chan T
 
-func (f PipeFunc[T]) Pipe(ctx context.Context, in <-chan T) <-chan T {
+func (f StageFunc[T]) Pipe(ctx context.Context, in <-chan T) <-chan T {
 	return f(ctx, in)
 }
 
-// A Pipeline allows the output of one Pipe to be used as the input of another.
+// A Pipeline allows the output of one Stage to be used as the input of another.
 type Pipeline[T any] struct {
-	// Cap determines maximum queue size for each pipe.
+	// Cap determines maximum queue size for aggregating channels.
 	Cap int
 
-	pipes []pipeInfo[T]
+	stages []stageInfo[T]
 }
 
-type pipeInfo[T any] struct {
-	n    int
-	pipe Pipe[T]
+type stageInfo[T any] struct {
+	n     int
+	stage Stage[T]
 }
 
-// Append appends n parallel jobs of pipe to pl.
-func (pl *Pipeline[T]) Append(n int, pipe Pipe[T]) {
+// Append appends n parallel jobs of stage to pl.
+func (pl *Pipeline[T]) Append(n int, stage Stage[T]) {
 	if n <= 0 {
-		panic("pipeline error: non-positive pipe jobs")
+		panic("pipeline error: non-positive number of stage jobs")
 	}
 
-	pl.pipes = append(pl.pipes, pipeInfo[T]{n, pipe})
+	pl.stages = append(pl.stages, stageInfo[T]{n, stage})
 }
 
-// AppendFunc appends n parallel jobs of pipe to pl.
-func (pl *Pipeline[T]) AppendFunc(n int, pipe PipeFunc[T]) {
-	pl.Append(n, pipe)
+// AppendFunc appends n parallel jobs of stage to pl.
+func (pl *Pipeline[T]) AppendFunc(n int, stage StageFunc[T]) {
+	pl.Append(n, stage)
 }
 
-// Clear removes all pipes from pl.
+// Clear removes all stages from pl.
 func (pl *Pipeline[T]) Clear() {
-	pl.pipes = nil
+	pl.stages = nil
 }
 
 func (pl *Pipeline[T]) Pipe(ctx context.Context, in <-chan T) <-chan T {
-	for _, info := range pl.pipes {
+	for _, info := range pl.stages {
 		outs := make([]<-chan T, info.n)
 		for i := range info.n {
-			outs[i] = info.pipe.Pipe(ctx, in)
+			outs[i] = info.stage.Pipe(ctx, in)
 		}
 
 		in = Aggregate(pl.Cap, outs)
