15100010bec6746d91b85b8e77c1a52fd7d48611
Amirabbas Fazelinia <utilyre@gmail.com>
2025-01-12 10:49:22 +0200

    test(benchmark): rename binaries to match version corresponding versions

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 hyperfine -w 3 --export-markdown=results.md --export-json=results.json \
-	'./summer1 database' \
-	'./summer2 generate -r database' \
-	'./summer2 generate -r --read-jobs=8 --digest-jobs=8 database'
+	'./summer-v0.1.0 database' \
+	'./summer-v0.8.0 generate -r database' \
+	'./summer-v0.8.0 generate -r --read-jobs=8 --digest-jobs=8 database'
